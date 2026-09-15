e03fd190b58656336555019d6d2f2133b27876f4
Ayush kumar Mishra <ayushrajmishra960@gmail.com>
2026-09-15 19:41:57 +0530

    updated css

--- frontend/src/components/LoginLeftSide.jsx
+++ frontend/src/components/LoginLeftSide.jsx
@@ -2,9 +2,7 @@ const LoginLeftSide = () => {
   return (
     <div className="hidden shrink-0 select-none flex-col justify-between bg-indigo-700 p-12 lg:flex lg:w-2/5">
       <div className="flex items-center gap-3">
-        <div className="grid h-14 w-14 place-items-center rounded-lg bg-white text-2xl font-bold text-indigo-700">
-          B
-        </div>
+        <svg width="74" height="64" viewBox="0 0 74 64" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M38.643 30.188C38.643 13.516 52.158 0 68.83 0h4.83c0 16.673-13.516 30.188-30.188 30.188z" fill="#edb731"/><path d="M38.643 33.812C38.643 50.484 52.158 64 68.83 64h4.83c0-16.672-13.516-30.188-30.188-30.188z" fill="#7443fd"/><path d="M35.019 30.188C35.019 13.516 21.503 0 4.83 0H0c0 16.673 13.516 30.188 30.19 30.188z" fill="#ef345d"/><path d="M35.019 33.812C35.019 50.484 21.503 64 4.83 64H0c0-16.672 13.516-30.188 30.19-30.188z" fill="#0bdaac"/></svg>
         <span className="text-4xl font-medium text-white">
           Bookstore Management System
         </span>
